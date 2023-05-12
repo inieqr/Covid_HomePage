5d54e459748c5fdea874cf34e10fef992fb3db3d
inieqr <70246193+inieqr@users.noreply.github.com>
2023-05-12 10:37:55 +0100

    Update app.py

--- app.py
+++ app.py
@@ -1,5 +1,4 @@
 import streamlit as st
-from link_button import link_button
 
 st.markdown("<h1 style='text-align: center; color: black;'>COVID-19 Prediction App</h1>", unsafe_allow_html=True)
 
@@ -12,7 +11,26 @@ st.image("https://upload.wikimedia.org/wikipedia/commons/8/82/SARS-CoV-2_without
 
 # Add a button
 # st.button("Click here to go to Google", href="https://www.google.com")
-link_button('Click Me!', 'https://docs.streamlit.io/en/stable/')
+def get_st_button_a_tag(https://predictcovid.streamlit.app/, Click To Predict):
+    """
+    generate html a tag
+    :param url_link:
+    :param button_name:
+    :return:
+    """
+    return f'''
+    <a href={https://predictcovid.streamlit.app/}><button style="
+    fontWeight: 400;
+    padding: 0.25rem 0.75rem;
+    borderRadius: 0.25rem;
+    margin: 0px;
+    lineHeight: 1.6;
+    width: auto;
+    userSelect: none;
+    backgroundColor: #FFFFFF;
+    border: 1px solid rgba(49, 51, 63, 0.2);">{CLick To Predict}</button></a>
+    '''
+st.markdown(get_st_button_a_tag('https://predictcovid.streamlit.app/', 'Click To Predict'), unsafe_allow_html=True)
 
 # Display the page
 st.show()
