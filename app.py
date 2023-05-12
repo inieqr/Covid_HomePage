14eecdfea221d823e37b39b0d96506503a96100e
inieqr <70246193+inieqr@users.noreply.github.com>
2023-05-12 10:21:47 +0100

    Update app.py

--- app.py
+++ app.py
@@ -1,8 +1,12 @@
 import streamlit as st
 
-# Set the page title
-with st.container():
-  st.title("COVID-19 Prediction App")
+st.markdown("<h1 style='text-align: center; color: grey;'>COVID-19 Prediction App</h1>", unsafe_allow_html=True)
+
+
+
+# # Set the page title
+# with st.container():
+#   st.title("COVID-19 Prediction App")
 
 # Add a picture
 st.image("https://upload.wikimedia.org/wikipedia/commons/8/82/SARS-CoV-2_without_background.png")
