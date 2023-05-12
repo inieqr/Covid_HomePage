6fb87c88e2c5084cc7b8abff9da8ffc73a481883
inieqr <70246193+inieqr@users.noreply.github.com>
2023-05-12 10:44:04 +0100

    Update app.py

--- app.py
+++ app.py
@@ -1,4 +1,5 @@
 import streamlit as st
+from streamlit.components.v1 import html
 
 st.markdown("<h1 style='text-align: center; color: black;'>COVID-19 Prediction App</h1>", unsafe_allow_html=True)
 
@@ -11,26 +12,16 @@ st.image("https://upload.wikimedia.org/wikipedia/commons/8/82/SARS-CoV-2_without
 
 # Add a button
 # st.button("Click here to go to Google", href="https://www.google.com")
-def get_st_button_a_tag(url_link, button_name):
-    """
-    generate html a tag
-    :param url_link:
-    :param button_name:
-    :return:
-    """
-    return f'''
-    <a href={https://predictcovid.streamlit.app/}><button style="
-    fontWeight: 400;
-    padding: 0.25rem 0.75rem;
-    borderRadius: 0.25rem;
-    margin: 0px;
-    lineHeight: 1.6;
-    width: auto;
-    userSelect: none;
-    backgroundColor: #FFFFFF;
-    border: 1px solid rgba(49, 51, 63, 0.2);">{Predict}</button></a>
-    '''
-st.markdown(get_st_button_a_tag('https://predictcovid.streamlit.app/', 'Predict'), unsafe_allow_html=True)
+
+def open_page(url):
+    open_script= """
+        <script type="text/javascript">
+            window.open('%s', '_blank').focus();
+        </script>
+    """ % (url)
+    html(open_script)
+
+st.button('Open link', on_click=open_page, args=('https://streamlit.io',))
 
 # Display the page
 st.show()
