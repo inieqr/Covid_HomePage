98a6cc4abc2c397f555f9c1e316e337560fc05a1
inieqr <70246193+inieqr@users.noreply.github.com>
2023-05-12 11:45:22 +0100

    Update app.py

--- app.py
+++ app.py
@@ -10,9 +10,15 @@ st.markdown("<h1 style='text-align: center; color: #2a2b2a;'>COVID-19 Prediction
 # Add a picture
 st.image("https://upload.wikimedia.org/wikipedia/commons/8/82/SARS-CoV-2_without_background.png")
 
-# Add a button
-# st.button("Click here to go to Google", href="https://www.google.com")
+# Add textual information
+st.markdown("<h1 style='text-align: center; color: red;'>What Is Covid-19</h1>", unsafe_allow_html=True)
+st.markdown("<h2 style='text-align: center; color: #2a2b2a;'>Coronavirus</h1>", unsafe_allow_html=True)
 
+
+st.write("Coronaviruses are a large family of viruses that are known to cause diseases ranging from colds to more severe diseases such as Middle Eastern respiratory syndrome (MERS) and severe acute respiratory syndrome (SARS). Symptoms of COVID-19 are similar to those of a cold at first. However, the disease can cause severe pneumonia, which can be fatal.")
+
+
+# Add a button
 def open_page(url):
     open_script= """
         <script type="text/javascript">
