2a4537af60753137d24314cd02453e4d35f1a1be
inieqr <70246193+inieqr@users.noreply.github.com>
2023-05-12 10:32:54 +0100

    Update app.py

--- app.py
+++ app.py
@@ -1,4 +1,5 @@
 import streamlit as st
+from link_button import link_button
 
 st.markdown("<h1 style='text-align: center; color: black;'>COVID-19 Prediction App</h1>", unsafe_allow_html=True)
 
@@ -10,7 +11,8 @@ st.markdown("<h1 style='text-align: center; color: black;'>COVID-19 Prediction A
 st.image("https://upload.wikimedia.org/wikipedia/commons/8/82/SARS-CoV-2_without_background.png")
 
 # Add a button
-st.button("Click here to go to Google", href="https://www.google.com")
+# st.button("Click here to go to Google", href="https://www.google.com")
+link_button('Click Me!', 'https://docs.streamlit.io/en/stable/')
 
 # Display the page
 st.show()
